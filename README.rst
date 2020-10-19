46b579faf2b4d96506a27a8dd34aeaf9b123c722
Konrad Knitter <klotek@gmail.com>
2020-10-19 22:48:03 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,9 @@
+pl_syllables
+=====================================================================
+
+Algorithm breaking down Polish words into syllables
+
+The Polish language is one of the most difficult languages of the world, not allowing to define algorithms that will work in all the cases. The best ones meet an accuracy of around 90%.
+
+The goal of this project is to as much accurate script, with a supporting database for exception words.
+
---
+++ pl_syllables/pl_syllables.py
@@ -0,0 +1,82 @@
+POLISH_VOVELS = ['a', 'ą', 'e', 'ę', 'i', 'o', 'u', 'ó', 'y']
+
+def count_sylables(word):
+    result = 0
+    for character in word:
+        if character in POLISH_VOVELS:
+            print(character)
+
+class WordSplitter():
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def count_sylables(self):
+        number_of_syllables = 0
+        for id, character in enumerate(self.word):
+            if character in POLISH_VOVELS:
+                if not ((id + 1) < len(self.word) and character == "i" and self.word[id+1] in POLISH_VOVELS):
+                    if not ((id + 1) < len(self.word) and self.word[id+1] == "u"):
+                        number_of_syllables = number_of_syllables + 1
+        return number_of_syllables
+
+    def to_syllables(self):
+        number_of_syllables = self.count_sylables()
+        if number_of_syllables > 1:
+            split_word = []
+            syllable_index = 0
+            syllable = ""
+            id = 0
+            while (id < len(self.word)):
+                character = self.word[id]
+                if character in POLISH_VOVELS:
+                    syllable_index = syllable_index + 1
+                    syllable = syllable + character
+
+                    if (id + 1) < len(self.word) and character == "i" and self.word[id + 1] in POLISH_VOVELS:
+                        id = id + 1
+                        syllable = syllable + self.word[id]
+
+                    if (id + 1) < len(self.word) and character == "u":
+                        id = id + 1
+                        syllable = syllable + self.word[id]
+
+                    if syllable_index == number_of_syllables:
+                        if id == (len(self.word) - 1):
+                            split_word.append(syllable)
+                            syllable = ""
+                        else:
+                            for sub_id, sub_character in enumerate(self.word[id+1:]):
+                                syllable = syllable + sub_character
+                                syllable = ""
+                                id = id + 1
+                    else:
+                        if (id + 2) < len(self.word) and self.word[id + 1] not in POLISH_VOVELS and self.word[id + 2] not in POLISH_VOVELS:
+                            if (self.word[id+1:id+3] in ("cz", "sz", "rz", "dz", "ch", "dź", "dż") or self.word[id + 1] in ("b", "s", "ś") or self.word[id + 2] in ("ł")):
+                                syllable = syllable
+                                split_word.append(syllable)
+                                syllable = ""
+                            else:
+                                if (id + 3) < len(self.word) and self.word[id+1:id+3] == "drz":
+                                    syllable = syllable
+                                    split_word.append(syllable)
+                                    syllable = ""
+                                else:
+                                    syllable = syllable + self.word[id + 1]
+                                    split_word.append(syllable)
+                                    syllable = ""
+                                    id = id + 1
+                        elif id == (len(self.word) - 1):
+                            split_word.append(syllable)
+                            syllable = ""
+                        else:
+                            syllable = syllable
+                            split_word.append(syllable)
+                            syllable = ""
+                else:
+                    syllable = syllable + character
+                id = id + 1
+            return split_word
+        else:
+            return [self.word]
+
+print(WordSplitter("epigalokatechina").to_syllables())
